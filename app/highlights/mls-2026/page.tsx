90a7eea131839647e8cb3fa6d35b32c188e5d3b8
Nishant Jangra <117847653+nishantjangra18@users.noreply.github.com>
2026-07-06 21:03:42 +0530

    feat: make MLS 2026 See All route render items in standard grid view

--- app/highlights/mls-2026/page.tsx
+++ app/highlights/mls-2026/page.tsx
@@ -1,12 +1,5 @@
-﻿import { MlsHighlightsSection } from "@/components/MlsHighlightsSection";
-import { Shell } from "@/components/Shell";
+import { CompetitionHighlightsPage } from "@/components/CompetitionHighlightsPage";
 
 export default function Mls2026HighlightsPage() {
-  return (
-    <Shell>
-      <div className="pb-12 pt-8">
-        <MlsHighlightsSection />
-      </div>
-    </Shell>
-  );
+  return <CompetitionHighlightsPage category="mls-2026" />;
 }
